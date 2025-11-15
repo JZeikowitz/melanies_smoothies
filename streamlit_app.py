222596a67e45f537a858ac3a332fa774523bfee0
Jason Zeikowitz <72943494+JZeikowitz@users.noreply.github.com>
2025-11-14 21:37:06 -0600

    Update streamlit_app.py
    Commented out ingredients_string = ingredients_string[:-2], to match the format for DORA

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list:
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
-    ingredients_string = ingredients_string[:-2]
+    #ingredients_string = ingredients_string[:-2]
 
     name_insert_stmt = """ """
 
